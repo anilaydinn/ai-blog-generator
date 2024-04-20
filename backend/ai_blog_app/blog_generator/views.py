fa10abacaa36d5575cfcc9c06f1058f3e64e86b3
Anil Aydin <anlaydinn22@gmail.com>
2024-04-20 19:26:31 +0300

    Changelog: - Add login_required decorator to index

--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'index.html')
 
@@ -43,4 +45,5 @@ def user_signup(request):
     return render(request, 'signup.html')
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect('/')
